2a01deb7fd1e7164e64806ffb0b7502b525845e9
Jessie <jessie.griffin@icloud.com>
2020-08-02 13:49:37 -0400

    Adds functionality for Game Emulator ViewController

---
+++ PlayersApp/GameController.swift
@@ -0,0 +1,16 @@
+//
+//  GameController.swift
+//  PlayersApp
+//
+//  Created by Jessie Ann Griffin on 8/2/20.
+//  Copyright © 2020 Jessie Griffin. All rights reserved.
+//
+
+import Foundation
+
+class GameController {
+    var playerOne: Player?
+    var playerTwo: Player?
+
+    
+}
---
+++ PlayersApp/GameEmulatorViewController.swift
@@ -0,0 +1,95 @@
+//
+//  GameEmulatorViewController.swift
+//  PlayersApp
+//
+//  Created by Jessie Ann Griffin on 8/2/20.
+//  Copyright © 2020 Jessie Griffin. All rights reserved.
+//
+
+import UIKit
+
+class GameEmulatorViewController: UIViewController {
+
+    let cornerRadius = 20
+
+    @IBOutlet weak var playerOneLabel: UILabel!
+    @IBOutlet weak var playerTwoLabel: UILabel!
+    @IBOutlet weak var playerOneWinsButton: UIButton!
+    @IBOutlet weak var playerTwoWinsButton: UIButton!
+    @IBOutlet weak var tieButton: UIButton!
+
+    let gameController = GameController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        playerOneWinsButton.layer.cornerRadius = CGFloat(cornerRadius)
+        playerTwoWinsButton.layer.cornerRadius = CGFloat(cornerRadius)
+        tieButton.layer.cornerRadius = CGFloat(cornerRadius)
+        updateViews()
+    }
+    
+    @IBAction func player1WinsPressed(_ sender: UIButton) {
+        updateScores(sender)
+    }
+
+    @IBAction func player2WinsPressed(_ sender: UIButton) {
+        updateScores(sender)
+    }
+
+    @IBAction func itsATiePressed(_ sender: UIButton) {
+        updateScores(sender)
+    }
+
+    private func updateViews() {
+        guard let playerOne = gameController.playerOne?.name,
+            let playerTwo = gameController.playerTwo?.name else { return }
+
+        playerOneLabel.text = playerOne
+        playerTwoLabel.text = playerTwo
+
+        playerOneWinsButton.titleLabel?.text = "\(playerOne) Wins!"
+        playerTwoWinsButton.titleLabel?.text = "\(playerTwo) Wins!"
+        tieButton.titleLabel?.text = "It's a tie!"
+
+    }
+
+    func showAlert(player: String) {
+        let alertController = UIAlertController(
+            title: "Missing player!",
+            message: "PLease choose \(player).",
+            preferredStyle: .alert)
+        let alertAction = UIAlertAction(
+            title: "OK",
+            style: UIAlertAction.Style.default,
+            handler: { _ in
+                self.navigationController?.popToRootViewController(animated: true)
+        })
+
+        alertController.addAction(alertAction)
+        self.present(alertController, animated: true)
+    }
+    func updateScores(_ sender: UIButton) {
+        guard let playerOne = gameController.playerOne else {
+            self.showAlert(player: "player one")
+            return
+        }
+        guard let playerTwo = gameController.playerTwo else {
+            self.showAlert(player: "player two")
+            return
+        }
+
+        switch sender {
+        case playerOneWinsButton:
+            playerOne.wins += 1
+            playerTwo.losses += 1
+        case playerTwoWinsButton:
+            playerTwo.wins += 1
+            playerOne.losses += 1
+        case tieButton:
+            playerOne.ties += 1
+            playerTwo.ties += 1
+        default:
+            break
+        }
+    }
+}
